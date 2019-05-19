9a9edfbe4de6a3bfcc3d28178258c514a3568473
justinmmott <justinmmott@gmail.com>
2019-05-19 03:20:29 -0400

    LSTM is working need to add more args

---
+++ lstm/Lstm-Aesthetic.py
@@ -0,0 +1,173 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import argparse
+import random
+from datetime import datetime
+from tqdm import tqdm
+# import sys
+# import getopt
+
+random.seed(datetime.now())
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--niter', type=int, default=300,
+                    help='number of epochs to train for')
+parser.add_argument('--cuda', action='store_true', help='enables cuda')
+parser.add_argument('--edim', type=int, default=32,
+                    help='number of embedding dimensions')
+parser.add_argument('--hdim', type=int, default=32,
+                    help='number of hidden dimensions')
+parser.add_argument('--tsize', type=int, default=100,
+                    help='number of training sets create per level')
+parser.add_argument('--prob', type=int, default=30,
+                    help='prob a pipe tile is changed')
+opt = parser.parse_args()
+
+assert torch.cuda.is_available()
+torch.cuda.set_device(0)
+
+
+def prepare_sequence(seq, to_ix):
+    idxs = [to_ix[w] for w in seq]
+    res = torch.tensor(idxs, dtype=torch.long).cuda()
+    return res
+
+
+# numArg = len(sys.argv)
+# levelByRow = [[] for i in range(numArg)]
+# for i in range(numArg):
+
+with open("mario-1-1.txt") as textFile:
+    levelByRow = [list(line) for line in textFile]
+# We read in a text file version of SMB1 level 1-1 into a 2-D array
+# However, the LSTM will read the level column by column
+# So, it is necessary to swap rows and columns
+levelByColumn = ""
+for j in range(len(levelByRow[0])):
+    for i in range(len(levelByRow)):
+        levelByColumn += str(levelByRow[i][j]) + " "
+levelByColumnArray = levelByColumn.split()
+training_data = [(levelByColumnArray, levelByColumnArray)]
+
+pipe = ["<", ">", "[", "]"]
+pieces = ["X", "-", "<", ">", "[", "]"]
+piecesFull = ["X", "S", "-", "?", "Q", "E", "<", ">", "[", "]"]
+tileMapping = {"X": 0, "S": 1, "-": 2, "?": 3, "Q": 4,
+               "E": 5, "<": 6, ">": 7, "[": 8, "]": 9}
+
+# Create Training Data
+numTiles = [0 for i in range(10)]
+for k in range(opt.tsize):
+    proturbedLevel = ""
+    for i in range(len(levelByColumnArray)):
+        numTiles[tileMapping[levelByColumnArray[i]]] += 1
+        if levelByColumnArray[i] in pipe and random.randint(0, 99) < opt.prob:
+            proturbedLevel += random.choice(piecesFull) + " "
+        else:
+            proturbedLevel += levelByColumnArray[i] + " "
+    training_data.append((proturbedLevel.split(), levelByColumnArray))
+print(numTiles)
+EMBEDDING_DIM = opt.edim
+HIDDEN_DIM = opt.hdim
+
+
+class LSTMTagger(nn.Module):
+
+    def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size):
+
+        super(LSTMTagger, self).__init__()
+        self.hidden_dim = hidden_dim
+
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+
+        # The LSTM takes word embeddings as inputs, and outputs hidden states
+        # with dimensionality hidden_dim.
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+
+        # The linear layer that maps from hidden state space to tag space
+        self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
+
+    def forward(self, sentence):
+        embeds = self.word_embeddings(sentence)
+        lstm_out, _ = self.lstm(embeds.view(len(sentence), 1, -1))
+        tag_space = self.hidden2tag(lstm_out.view(len(sentence), -1))
+        tag_scores = F.log_softmax(tag_space, dim=1)
+        return tag_scores
+
+
+model = LSTMTagger(EMBEDDING_DIM, HIDDEN_DIM, len(tileMapping),
+                   len(tileMapping))
+model.cuda()
+loss_function = nn.NLLLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.1)
+
+for epoch in tqdm(range(opt.niter)):
+    for sentence, tags in training_data:
+        # Step 1. Remember that Pytorch accumulates gradients.
+        # We need to clear them out before each instance
+        model.zero_grad()
+
+        # Step 2. Get our inputs ready for the network
+        sentence_in = prepare_sequence(sentence, tileMapping)
+        targets = prepare_sequence(tags, tileMapping)
+
+        # Step 3. Run our forward pass.
+        tag_scores = model(sentence_in)
+
+        # Step 4. Compute the loss, gradients, and update the parameters by
+        #  calling optimizer.step()
+        loss = loss_function(tag_scores, targets)
+        loss.backward()
+        optimizer.step()
+
+# Find training accuracy
+with torch.no_grad():
+    correct = 0
+    for i in range(len(training_data)):
+        inputs = prepare_sequence(training_data[i][0], tileMapping)
+        tag_scores = model(inputs)
+        values, indices = torch.max(tag_scores, 1)
+        for j in range(len(indices)):
+            if indices[j].item() == tileMapping[levelByColumnArray[j]]:
+                correct += 1
+    total = len(training_data) * len(indices)
+    accuracy = (correct / total) * 100
+    print("Training accuracy " + str(round(accuracy, 2)) + "%")
+
+# Find Test accuracy
+with torch.no_grad():
+    testing_data = []
+    for k in range(opt.tsize):
+        proturbedLevel = ""
+        for i in range(len(levelByColumn)):
+            if levelByColumn[i] in pipe and random.randint(0, 99) < opt.prob:
+                proturbedLevel += random.choice(piecesFull) + " "
+            else:
+                proturbedLevel += levelByColumn[i] + " "
+        testing_data.append((proturbedLevel.split(), levelByColumnArray))
+    correct = 0
+    fullCorrect = 0
+    tilesIncorrect = [[0 for i in range(10)] for j in range(10)]
+    for i in range(len(testing_data)):
+        inputs = prepare_sequence(testing_data[i][0], tileMapping)
+        tag_scores = model(inputs)
+        values, indices = torch.max(tag_scores, 1)
+        isCorrect = True
+        for j in range(len(indices)):
+            if indices[j].item() == tileMapping[levelByColumnArray[j]]:
+                correct += 1
+            else:
+                isCorrect = False
+                tilesIncorrect[tileMapping[levelByColumnArray[j]]][indices[j].item()] += 1
+        if isCorrect:
+            fullCorrect += 1
+    total = len(testing_data) * len(indices)
+    accuracy = (correct / total) * 100
+    print("Testing accuracy " + str(round(accuracy, 2)) + "%")
+    fullAccuracy = (fullCorrect / len(testing_data)) * 100
+    print("Testing levels that were fully correct: " +
+          str(round(fullAccuracy, 2)) + "%")
+    for row in tilesIncorrect:
+        print(row)
