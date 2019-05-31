0b59455d291eb61379b3b806a5db7776aa9fc798
Justin <justinmmott@gmail.com>
2019-05-31 12:48:40 -0700

    on big boi

--- lstm/Seq2Seq-Aesthetic.py
+++ lstm/Seq2Seq-Aesthetic.py
@@ -13,15 +13,19 @@ random.seed(datetime.now())
 
 parser = argparse.ArgumentParser()
 parser.add_argument('directory')
-parser.add_argument('--niter', type=int, default=10,
+parser.add_argument('--niter', type=int, default=100,
                     help='number of epochs to train for')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
+parser.add_argument('--edim', type=int, default=256,
+                    help='number of embedding dimensions')
 parser.add_argument('--hdim', type=int, default=256,
                     help='number of hidden dimensions')
-parser.add_argument('--tsize', type=int, default=10,
+parser.add_argument('--tsize', type=int, default=100,
                     help='number of training sets create per level')
-parser.add_argument('--prob', type=int, default=30,
+parser.add_argument('--probp', type=int, default=30,
                     help='prob a pipe tile is changed')
+parser.add_argument('--probs', type=int, default=30,
+                    help='prob a stair tile is changed')
 opt = parser.parse_args()
 
 assert torch.cuda.is_available()
