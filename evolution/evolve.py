c646c337be62354f5ad2d48e07188c375d13dbd7
s-nandi <saujas.nandi@gmail.com>
2019-07-19 09:16:38 -0400

    Speed up fitness functions

--- evolution/evolve.py
+++ evolution/evolve.py
@@ -1,83 +1,97 @@
 import functools
 import math
-from multiprocessing import Pool
 import os
 import cma
 
 from common.constants import DEBUG_PRINT, INF
 from evolution.level_difficulty.difficulty \
     import calculate_difficulty_for_failure, calculate_difficulty_for_success
-from common.simulate_agent import simulate_level_with_astar
+from common.simulate_agent \
+    import simulate_level_with_astar
 from gan import generator_client
+from multiprocessing import Pool
 
 from typing import NamedTuple
 
 class Hyperparameters(NamedTuple):
     SUCCESS_COEFFICIENT : float = 0.0025
-    FAILURE_COEFFICIENT : float = 1.0000
+    FAILURE_PERCENTAGE_COEFFICIENT : float = 1.0000
     ALL_FAILURE_COEFFICIENT : float = 1.0000
 
 # Number of times the A* agent is invoked on each sample during evolution
-TRIALS_PER_SAMPLE = 1
+TRIALS_PER_SAMPLE = 5
 MAX_ITERS = 1
-
-# TODO: Refactor into class for cleanliness
 
 ### WARNING: CONSTRUCTION ZONE ###
 
 cma_es = None
 
 # Sample fitness function based on EvalutionInfo information
-def _fitness(level, hp, ret_passed_bool = False):
+def _fitness(level):
     info = simulate_level_with_astar(level)
     passed = info.level_passed()
-    
+    if DEBUG_PRINT:
+        print("Passed:", passed)
     if not passed:
-        difficulty = calculate_difficulty_for_failure(info)
+        return _fitness_failure(info, level)
     else:
-        difficulty = calculate_difficulty_for_success(info, level)
-    fitness = -difficulty if passed else difficulty
+        return _fitness_success(info, level)
     
-    if ret_passed_bool:
-        return fitness, passed
-    else:
-        return fitness
+def _fitness_success(eval_info, level):
+    assert eval_info.level_passed()
+    difficulty = calculate_difficulty_for_success(eval_info, level)
+    fitness = -difficulty # Negate difficulty since fitness is minimized
+    return fitness
+    
+def _fitness_failure(eval_info, level):
+    assert not eval_info.level_passed()
+    difficulty = calculate_difficulty_for_failure(eval_info)
+    fitness = difficulty
+    return fitness
 
 def _multiple_run_fitness(level, hp):
-    min_fit = INF
-    fits = []
-    passed = []
-
-    for t_itr in range(TRIALS_PER_SAMPLE):
-        trial_fit, level_passed = _fitness(level, hp, ret_passed_bool = True)
-        
-        if DEBUG_PRINT:
-            print("Trial " + str(t_itr) + " Fitness: " + str(trial_fit))
+    """ 
+    First, astar agent is run TRIALS_PER_SAMPLE times on the given level
     
-        fits.append(trial_fit)
-        passed.append(1 if level_passed else 0)
-        min_fit = min(trial_fit, min_fit)
+    If the agent passes the level at least once, we use the agent's eval info to
+        calculate the success fitness using the '_fitness' function
+        Afterwards we weigh this success fitness along with a reward for each
+        time that the astar agent failed
+        We add these weighted values to get the final level fitness value
+    If the astar agent always fails, we find the average fitness value of
+        failure and weigh it to get the final level fitness value
+    """
+    success_info = None
+    sum_failure_fitness = 0
+    failure_cnt = 0
 
-    passed_cnt = sum(passed)
-    failed_pct = float(TRIALS_PER_SAMPLE - passed_cnt) / TRIALS_PER_SAMPLE
-    ### TEST ###
-    if DEBUG_PRINT:
-        print("PASSED COUNT: " + str(passed_cnt))
-    
-    if passed_cnt > 0:
-        avg_passed_fit = sum([fits[i] * passed[i] for i in range(TRIALS_PER_SAMPLE)]) / passed_cnt
-        
-        scaled_fitness_value = hp.SUCCESS_COEFFICIENT * avg_passed_fit
-        scaled_repeat_reward = -1 * hp.FAILURE_COEFFICIENT * failed_pct
+    for _ in range(TRIALS_PER_SAMPLE):
+        info = simulate_level_with_astar(level)
         if DEBUG_PRINT:
-            print("AVG PASSED FIT: " + str(avg_passed_fit))
-        return scaled_fitness_value + scaled_repeat_reward
+            print("Level passed:", info.level_passed())
+        if info.level_passed():
+            success_info = info
+        else:
+            failure_cnt += 1
+            sum_failure_fitness += _fitness_failure(info, level)
+            
+    all_failures = failure_cnt == TRIALS_PER_SAMPLE
+    if all_failures:
+        average_failure_fitness = float(sum_failure_fitness) / failure_cnt
+        weighted_fitness_value = hp.ALL_FAILURE_COEFFICIENT * average_failure_fitness
+        if DEBUG_PRINT:
+            print("All failed")
+            print("Average failure fitness:", average_failure_fitness)
     else:
-        avg_fit = sum(fits) / TRIALS_PER_SAMPLE
+        failure_percentage = float(failure_cnt) / TRIALS_PER_SAMPLE
+        success_fitness = _fitness_success(success_info, level)
+        weighted_fitness_value = hp.SUCCESS_COEFFICIENT * success_fitness - \
+                                 hp.FAILURE_PERCENTAGE_COEFFICIENT * failure_percentage
         if DEBUG_PRINT:
-            print("AVG FIT: " + str(avg_fit))
-        scaled_fitness_value = hp.ALL_FAILURE_COEFFICIENT * avg_fit
-        return scaled_fitness_value
+            print("Not all failed")
+            print("Failure percentage:", failure_percentage)
+            print("Success fitness:", success_fitness)
+    return weighted_fitness_value
     
 def _latent_vector_fitness(latent_vector, hp):
     generator_client.load_generator()
@@ -100,12 +114,15 @@ def run(hyperparameters):
         p_sz = len(population)
         best_lv = None
         best_fitness = INF
-        with Pool() as p:
-            fits = list(map(fitness, population))
+        
+        with Pool() as pool:
+            fits = list(pool.map(fitness, population))          
             if DEBUG_PRINT:
+                print("Fits:", fits)
                 print(" ---- Generation " + str(gen_itr) + " ----")
                 print("GEN FITS: " + str(fits))
                 print("GEN AVG: " + str(sum(fits) / len(fits)))
+                print("p_sz", p_sz, "len_fits", len(fits))
             avg_fits.append(sum(fits) / len(fits))
             cma_es.tell(population, fits)
             for i in range(p_sz):
